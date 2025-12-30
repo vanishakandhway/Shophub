f7fc95cd6f64cf888731ca45ce3022246c09a672
vanishakandhway <vanishakandhway@gmail.com>
2025-12-30 12:34:41 +0530

    Make the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -80,7 +80,14 @@ if(matchingItem){
     quantity:1
    });
 }
-   console.log(cart);
+
+let cartQuantity =0;
+cart.forEach((item)=>{
+ cartQuantity+=item.quantity;
+});
+
+document.querySelector('.js-cart-quantity').innerHTML= cartQuantity;
+   
   });
 });
 
