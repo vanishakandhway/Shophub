5635e9d99928ef1c9e6360980ceacab82b33247b
vanishakandhway <vanishakandhway@gmail.com>
2026-01-02 23:43:57 +0530

    Make payment summary interactive

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -3,12 +3,11 @@ import{ products,getProduct } from '../../data/products.js';
 import { formatCurrency } from '../utils/money.js';
 import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
 import { deliveryOptions, getDeliveryOption } from '../../data/deliveryOptions.js';
+import { renderPaymentSummary } from './paymentSummary.js';
 
 
 
-const today=dayjs();
-const deliveryDate = today.add(7 , 'days');
-console.log(deliveryDate.format('dddd, MMMM D'));
+
 
 
 export function renderOrderSummary(){
@@ -135,6 +134,7 @@ document.querySelectorAll('.js-delete-link').forEach((link)=>{
          const productId= link.dataset.productId;
          removeFromCart(productId);
          
+         renderPaymentSummary();
 
         const container= document.querySelector(`.js-cart-item-container-${productId}`);
 
@@ -194,6 +194,8 @@ document.querySelectorAll('.js-update-link')
       quantityLabel.innerHTML = newQuantity;
 
       updateCartQuantity();
+      renderPaymentSummary();
+      
     });
   });
 
@@ -205,6 +207,7 @@ document.querySelectorAll('.js-update-link')
         updateDeliveryOption(productId,deliveryOptionId)
 
         renderOrderSummary();
+        renderPaymentSummary();
      });
   });
 
