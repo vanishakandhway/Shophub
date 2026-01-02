9b6a0e79989413d2be1cd191626172e2b73a333b
vanishakandhway <vanishakandhway@gmail.com>
2026-01-02 14:05:27 +0530

    Make delivery options interactive

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import {cart , removeFromCart,calculateCartQuantity,updateQuantity} from '../data/cart.js';
+import {cart , removeFromCart,calculateCartQuantity,updateQuantity,updateDeliveryOption} from '../data/cart.js';
 import{ products} from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
 import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
@@ -115,7 +115,9 @@ function deliveryOptionsHTML(matchingProduct,cartItem){
         const isChecked = deliveryOption.id === cartItem.deliveryOptionId;
 
        html+=`
-        <div class="delivery-option">
+        <div class="delivery-option js-delivery-option"
+        data-product-id="${matchingProduct.id}"
+        data-delivery-option-id="${deliveryOption.id}">
         <input type="radio"
         ${isChecked ?'checked':''}
         class="delivery-option-input"
@@ -203,4 +205,13 @@ document.querySelectorAll('.js-update-link')
 
       updateCartQuantity();
     });
+  });
+
+
+  document.querySelectorAll('.js-delivery-option')
+  .forEach((element)=>{
+     element.addEventListener('click',()=>{
+        const {productId,deliveryOptionId}=element.dataset;
+        updateDeliveryOption(productId,deliveryOptionId)
+     });
   });
